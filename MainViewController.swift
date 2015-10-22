7bfda5b92e884856281c97a5ca11ed200ca02212
Dani Pletter <dani@fawnapp.com>
2015-10-22 22:54:37 +0200

    dot

---
+++ MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewController.swift
+//  Phototrope
+//
+//  Created by Dani Pletter on 10/22/15.
+//  Copyright © 2015 Everyside Innovations, LLC. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
